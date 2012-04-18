a3e2838dda81a016cb41d83061673243ec6443ee
root <root@bt.foo.org>
2012-04-17 20:08:07 -0500

    added utils

---
+++ UTIL/utils.cpp
@@ -0,0 +1,67 @@
+#include <string>
+
+using namespace std;
+
+string itob(int n, int l)
+{
+    bool neg;
+    string result;
+
+    if (n < 0)
+        neg = true;
+    else
+        neg = false;
+
+    if (neg)
+    {
+        //do 2's complement
+        n = ~n;
+        n++;
+    }
+
+    do result.push_back( '0' + (n & 1) );
+    while (n >>= 1);
+
+    reverse( result.begin(), result.end() );
+
+    //pad
+    if (result.length() < l)
+    {
+        for (int i = 0; i < (l - result.length()); i++)
+        {
+            if (neg)
+                result.insert(0,'1');
+            else
+                result.insert(0,'0');
+        }
+    }
+
+    if (result.length() > l)
+    //result overflow!
+    {
+        cout << "ERROR: binary representation of " << n << " bigger than " << l << " bits!" << endl;
+    }
+
+    return result;
+}
+
+int btoi (char* str)
+{
+    bool neg;
+    int retval;
+
+    if str[0] == '0'
+        neg = false;
+    else
+        neg = true;
+
+    retval = strtol(str,NULL,2);
+    if neg
+    {
+        //undo 2's complement
+        retval--;
+        retval = ~retval;
+    }
+
+    return retval;
+}
